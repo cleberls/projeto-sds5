625b11f697b9cefd6aa5331038e0d779268d957d
Cleber Sousa <cleberls@hotmail.com>
2021-11-07 11:05:46 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,10 @@
 import BarChart from "components/BarChart";
-import DataTable from "components/DataTable";
-import DonutsChart from "components/DonutsChart";
-import Footer from "components/Footer";
-import NavBar from "components/NavBars";
+import Routers from "Routes";
 
 
 function App() {
   return (
-    <>
-       <NavBar /> 
-       <div className="container">
-          <h1 className="text-primary py-3"> Dahsborad de vendas! </h1> 
-            
-            <div className="row px-3">
-              <div className="col-sm-6">
-                <h5 className="text-center text-secondary">(%) de Sucesso</h5>
-                <BarChart />
-              </div>
-
-              <div className="col-sm-6">
-              <h5 className="text-center text-secondary">Todas Vendas</h5>
-                <DonutsChart />
-              </div>
-            </div>
-            
-            <div className="py-3">
-              <h2 className="text-primary"> Todas Vendas </h2>
-            </div>
-
-          <DataTable /> 
-      </div>  
-      <Footer />
-    </>
+    <Routers />
   );
 }
 
---
+++ frontend/src/pages/Dashboard/index.tsx
@@ -0,0 +1,40 @@
+import BarChart from "components/BarChart";
+import DataTable from "components/DataTable";
+import DonutsChart from "components/DonutsChart";
+import Footer from "components/Footer";
+import NavBar from "components/NavBars";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <>
+       <NavBar /> 
+       <div className="container">
+          <h1 className="text-primary py-3"> Dahsborad de vendas! </h1> 
+            
+            <div className="row px-3">
+              <div className="col-sm-6">
+                <h5 className="text-center text-secondary">(%) de Sucesso</h5>
+                <BarChart />
+              </div>
+
+              <div className="col-sm-6">
+              <h5 className="text-center text-secondary">Todas Vendas</h5>
+                <DonutsChart />
+              </div>
+            </div>
+            
+            <div className="py-3">
+              <h2 className="text-primary"> Todas Vendas </h2>
+            </div>
+
+          <DataTable /> 
+      </div>  
+      <Footer />
+    </>
+    </div>
+  
+  );
+}
+
+export default Dashboard;
