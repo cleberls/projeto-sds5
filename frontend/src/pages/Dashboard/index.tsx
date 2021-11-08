f89430b0b3948a818247c266cef01a8a2953c183
Cleber Sousa <cleberls@hotmail.com>
2021-11-07 21:10:53 -0300

    DataTable integration

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,40 +1,39 @@
-import BarChart from "components/BarChart";
-import DataTable from "components/DataTable";
-import DonutsChart from "components/DonutsChart";
-import Footer from "components/Footer";
-import NavBar from "components/NavBars";
+import BarChart from 'components/BarChart';
+import DataTable from 'components/DataTable';
+import DonutsChart from 'components/DonutsChart';
+import Footer from 'components/Footer';
+import NavBar from 'components/NavBars';
 
 const Dashboard = () => {
   return (
     <div>
       <>
-       <NavBar /> 
-       <div className="container">
-          <h1 className="text-primary py-3"> Dahsborad de vendas! </h1> 
-            
-            <div className="row px-3">
-              <div className="col-sm-6">
-                <h5 className="text-center text-secondary">(%) de Sucesso</h5>
-                <BarChart />
-              </div>
+        <NavBar />
+        <div className="container">
+          <h1 className="text-primary py-3"> Dahsboard de vendas! </h1>
 
-              <div className="col-sm-6">
+          <div className="row px-3">
+            <div className="col-sm-6">
+              <h5 className="text-center text-secondary">(%) de Sucesso</h5>
+              <BarChart />
+            </div>
+
+            <div className="col-sm-6">
               <h5 className="text-center text-secondary">Todas Vendas</h5>
-                <DonutsChart />
-              </div>
+              <DonutsChart />
             </div>
-            
-            <div className="py-3">
-              <h2 className="text-primary"> Todas Vendas </h2>
-            </div>
+          </div>
 
-          <DataTable /> 
-      </div>  
-      <Footer />
-    </>
+          <div className="py-3">
+            <h2 className="text-primary"> Todas Vendas </h2>
+          </div>
+
+          <DataTable />
+        </div>
+        <Footer />
+      </>
     </div>
-  
   );
-}
+};
 
 export default Dashboard;
